313023ebb138560f6bcff7f9da2cef06452d8b32
takunoumi <1701163@st.asojuku.ac.jp>
2019-06-12 10:02:39 +0900

    add DaoBase & css

---
+++ MovieReservation/src/dao/DaoBase.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DaoBase {
+	protected Connection con = null;
+
+	public void connect() throws Exception{
+		if(con != null) {
+			//すでに接続済みの場合は何もしない
+			return;
+		}
+
+		//コネクションプールから値を受け取るための機構
+		InitialContext context = new InitialContext();
+
+		try {
+			//データベースの種類を指定する
+			String resourceName = "jdbc/MySQL";
+
+			//jndi(Java Naming and Directory Interface)を指定
+			String jndi = "java:comp/env/" + resourceName;
+
+			//データを格納するための機構を生成する
+			DataSource dataSource = (DataSource)context.lookup(jndi);
+
+			//接続する
+			con = dataSource.getConnection();
+		}catch(Exception e) {
+			throw e;
+		}
+	}
+}
