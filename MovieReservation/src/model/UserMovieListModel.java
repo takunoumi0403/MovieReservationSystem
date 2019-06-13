abe8178c7f51314c1f3465fa4105c75b4c199190
1701166 <1701166@st.asojuku.ac.jp>
2019-06-13 14:03:18 +0900

    //aa

---
+++ MovieReservation/src/model/UserMovieListModel.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class UserMovieListModel {
+
+	public List<MovieListBeans> getList(DATE date) throws Exception{
+
+		List<MovieListBeans> list = new ArrayList<MovieListBeans>();
+
+		MovieListDao dao = new MovieListDao();
+
+		try{
+			///////////////////////////////////
+			//DBの接続
+			dao.connect();
+
+			list = dao.getMovieList(date);
+
+		}catch(Exception e) {
+			//エラー発生した場合にコンソールにログを出力する
+			e.printStackTrace();
+			throw e;
+		}
+		finally {
+			dao.close();
+		}
+
+		return list;
+	}
+
+}
