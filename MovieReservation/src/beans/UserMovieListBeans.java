5c8365cbd5efdd55b82187176a2a23712da7e8d1
1701166 <1701166@st.asojuku.ac.jp>
2019-06-19 10:12:59 +0900

    UserMovieListBeans作成

--- MovieReservation/src/beans/UserMovieListBeans.java
+++ MovieReservation/src/beans/UserMovieListBeans.java
@@ -1,5 +1,67 @@
 package beans;
+import java.io.Serializable;
 
-public class UserMovieListBeans {
+public class UserMovieListBeans implements Serializable {
+
+	private String movieCode;
+	private String movieName;
+	private String movieTime;
+	private String movieAddress;
+	private String movieThnmbnail;
+	private String movieDescription;
+	private String showCode;
+	private String theaterCode;
+
+
+	public String getMovieCode() {
+		return movieCode;
+	}
+	public void setMovieCode(String movieCode) {
+		this.movieCode = movieCode;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getMovieTime() {
+		return movieTime;
+	}
+	public void setMovieTime(String movieTime) {
+		this.movieTime = movieTime;
+	}
+	public String getMovieAddress() {
+		return movieAddress;
+	}
+	public void setMovieAddress(String movieAddress) {
+		this.movieAddress = movieAddress;
+	}
+	public String getMovieThnmbnail() {
+		return movieThnmbnail;
+	}
+	public void setMovieThnmbnail(String movieThnmbnail) {
+		this.movieThnmbnail = movieThnmbnail;
+	}
+	public String getMovieDescription() {
+		return movieDescription;
+	}
+	public void setMovieDescription(String movieDescription) {
+		this.movieDescription = movieDescription;
+	}
+	public String getShowCode() {
+		return showCode;
+	}
+	public void setShowCode(String showCode) {
+		this.showCode = showCode;
+	}
+	public String getTheaterCode() {
+		return theaterCode;
+	}
+	public void setTheaterCode(String theaterCode) {
+		this.theaterCode = theaterCode;
+	}
+
+
 
 }
