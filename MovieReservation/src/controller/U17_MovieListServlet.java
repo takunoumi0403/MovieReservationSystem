7390e27bca9562aea162db5a5f3f97f31afafe96
takunoumi <1701163@st.asojuku.ac.jp>
2019-06-24 10:53:15 +0900

    変更

--- MovieReservation/src/controller/U17_MovieListServlet.java
+++ MovieReservation/src/controller/U17_MovieListServlet.java
@@ -21,59 +21,27 @@ public class U17_MovieListServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		//		HttpSession session = request.getSession();
-		//
-		//		LoginInfoBeans loginInfoBeans = new LoginInfoBeans();
-		//		loginInfoBeans = (LoginInfoBeans)session.getAttribute("loginInfo");
-		//
-		//		UserMovieModel userMovieModel = new UserMovieModel();
-		//		List<MovieListBeans> list;
-		//
-		//		list = userMovieModel.getList(date);//aaa
-
-<<<<<<< HEAD
 		HttpSession session = request.getSession();
 
 		UserInfoBeans userInfoBeans = new UserInfoBeans();
 		userInfoBeans = (UserInfoBeans)session.getAttribute("userInfoBeans");
-=======
-<<<<<<< HEAD
-//		HttpSession session = request.getSession();
-//
-//		LoginInfoBeans loginInfoBeans = new LoginInfoBeans();
-//		loginInfoBeans = (LoginInfoBeans)session.getAttribute("loginInfo");
-//
-//		UserMovieModel userMovieModel = new UserMovieModel();
-//		List<MovieListBeans> list;
-//
-//		list = userMovieModel.getList(date);//aaa
-=======
-		HttpSession session = request.getSession();
-
-		LoginInfoBeans loginInfoBeans = new LoginInfoBeans();
-		loginInfoBeans = (LoginInfoBeans)session.getAttribute("loginInfo");
->>>>>>> egoshi
 
 		UserMovieListModel userMovieModel = new UserMovieListModel();
 		List<UserMovieListBeans> list;
 
-<<<<<<< HEAD
 		try {
 			list = userMovieModel.getMovieList();
 		} catch (Exception e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-=======
-		list = userMovieModel.getMovieList();//aaa
->>>>>>> kunihiro
 
 
 
->>>>>>> egoshi
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/top.jsp");
 		dispatcher.forward(request, response);
+
+
 	}
 
 }
