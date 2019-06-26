b8cf97a6a6f369f502fdf3dbcc9c4c5dae198c08
takunoumi <1701163@st.asojuku.ac.jp>
2019-06-26 10:04:35 +0900

    Login 画面遷移

--- MovieReservation/src/controller/U01_LoginStartServlet.java
+++ MovieReservation/src/controller/U01_LoginStartServlet.java
@@ -14,7 +14,7 @@ public class U01_LoginStartServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
+		RequestDispatcher dispatcher  = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
 		dispatcher.forward(request, response);
 	}
 
